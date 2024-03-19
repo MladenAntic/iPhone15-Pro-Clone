bc845a866e4b2b8335c682d82a8d9aca34bebfcf
Mladen Antic <mladen.antic98@gmail.com>
2024-03-19 18:31:07 +0100

    Second Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
           iPhone 15 Pro
